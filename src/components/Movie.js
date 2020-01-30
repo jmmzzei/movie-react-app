238c0b60bdb99ea2d02d3d8d1829c3d0d4363f8c
jmm <josemmazzei@gmail.com>
2020-01-30 11:04:18 -0300

    Scaffold Movie Page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,26 @@
 import React from 'react'
 
-export default ({movieId}) => (
-    <div>Movie component: {movieId}</div>
-)
+import Navigation from './elements/Navigation'
+import MovieInfo from './elements/MovieInfo'
+import MovieInfoBar from './elements/MovieInfoBar'
+import Actor from './elements/Actor'
+import Grid from './elements/Grid'
+import Spinner from './elements/Spinner'
+import useMovieFetch from './hooks/useMovieFetch'
+
+export default ({ movieId }) => {
+    const [movie, loading, err] = useMovieFetch(movieId)
+    console.log(movie);
+    
+
+    return(
+    <>
+        <Navigation />
+        <MovieInfo />
+        <MovieInfoBar />
+        <Grid>
+            <Actor />
+        </Grid>
+        <Spinner />
+    </>
+)}
